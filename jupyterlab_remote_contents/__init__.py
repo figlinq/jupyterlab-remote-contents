5ea672b2caa6d5e6a1a13a91113e7003e0dcd7be
Przemek <pk@figlinq.com>
2025-01-07 22:46:03 +0100

    Remove UserContentsManager and related server extension code

--- jupyterlab_remote_contents/__init__.py
+++ jupyterlab_remote_contents/__init__.py
@@ -1,15 +1,4 @@
 from ._version import __version__
-from .contents_manager import UserContentsManager
-
 
 def _jupyter_labextension_paths():
     return [{"src": "labextension", "dest": "jupyterlab-remote-contents"}]
-
-
-def _jupyter_server_extension_paths():
-    return [{"module": "jupyterlab_remote_contents"}]
-
-
-def load_jupyter_server_extension(nb_app):
-    nb_app.log.info("Enabling UserContentsManager")
-    nb_app.web_app.settings["contents_manager_class"] = UserContentsManager
